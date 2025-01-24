1087990c5def4c174d37ba4058154f955c09464e
gh0st-nemesis <bl4ck4mi@gmail.com>
2025-01-24 19:16:01 +0100

    push

---
+++ chap3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Reaper - chapitre 3</title>
+    <link rel="stylesheet" href="../assets/css/chap2enquete.css">
+</head>
+<body>
+    <div class="glitch">
+        <p class="glitch-title">
+            Troisième meurtre le 6 octobre 1888, Elizabeth Stride<br/><br/>
+            Elle n'a pas eu de chance elle non plus mais tu deviens une menace et tu es seul
+            
+        </p>
+        
+    </div>
+    
+</body>
+</html>
---
+++ chap3/night/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Reaper - Night dream</title>
+    <link rel="stylesheet" href="../../assets/css/chap1nightdream.css">
+</head>
+<body>
+    <div class="glitch">
+        <p class="glitch-title">
+            Tu es une menace je dois te tuer avant que tu ne me trouves
+        </p>
+        <div id="timer">30</div>
+        <form id="code-form" method="post">
+            <input type="text" name="code" placeholder="Enter the code" required>
+            <!-- save&run -->
+            <button type="submit">Submit</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $code = $_POST['code'];
+            if (strtolower($code) === 'save&run') {
+                echo '<p class="success-message">Correct! You found the code.</p>';
+                header('Location: /chap3/day');
+            } else {
+                echo '<p class="error-message">Incorrect code. Try again.</p>';
+            }
+        }
+        ?>
+    </div>
+    <div class="kill-message"></div>
+    <div id="game-over-modal" class="modal">
+        <div class="modal-content">
+            <h2>Game Over : Jack vous a tué</h2>
+            <button onclick="location.reload()">Refresh</button>
+        </div>
+    </div>
+    <script src="../../assets/js/killmessage.js"></script>
+    <script src="../../assets/js/timer.js"></script>
+</body>
+</html>
