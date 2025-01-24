26c5efcda5bef2e2e9ed3892cdbe65aca44b8ecd
gh0st-nemesis <bl4ck4mi@gmail.com>
2025-01-24 19:30:46 +0100

    push

--- chap3/cipher3/index.php
+++ chap3/cipher3/index.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Reaper - Search</title>
+    <title>The Reaper - Cipher</title>
     
 </head>
 <body>
     <div class="glitch">
         <p class="glitch-title">
-            cipher 2 : 5KU1NLXVVDXF9BUVtKVwdLXFRDWVpBVFhKVlZLXFhDXFlBUVtKVwdLXFBDXVNBVFhKU1NLXABDXV9BUVtKVlBLXVRDXVNBVFhKU1NL
+            cipher 2 : XAJDXV9BUVtKU1FLXABDXVtBVVhKVwFLXVVDXVJBVV5KVlFLXFRDXVtBVFtKV1ZLXVNDWVhBUVtKVwdLXAdDXV5BVV5KVlFLXARDXV8=
         </p>
-        <a href="/chap3" class="next-chapter" style="z-index:2;">Go to Chapter 3</a>
+        <p>Bon courage avec le cipher ;)</p>
         
     </div>
     
