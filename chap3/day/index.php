8f40276667419bac24dfeda463dbf83eb7295e36
gh0st-nemesis <bl4ck4mi@gmail.com>
2025-01-24 19:20:54 +0100

    push

---
+++ chap3/day/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Reaper - chapitre 3</title>
+    <link rel="stylesheet" href="../../assets/css/chap3day.css">
+</head>
+<body>
+    <div class="glitch">
+        <p class="glitch-title">
+            Vous avez fait un cauchemar mais une lettre vous a été remis la nuit continuer sur /letter
+        </p>
+        
+    </div>
+    
+</body>
+</html>
